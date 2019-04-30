1c816d47de74158808b38e718fad8001045a2460
Jeny Susan Rajan <43616819+ceccs17d28@users.noreply.github.com>
2019-04-30 18:35:22 +0530

    Delete http.sh

--- http.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get install nginx      #install nginx
-cd /var/www
-ls
-sudo nano index.html       	#give html commands in html file to display in http server
-sudo /etc/init.d/nginx stop
-sudo /etc/init.d/apache2 start
-
-
-
