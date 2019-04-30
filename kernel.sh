c70672a993d93c0e98f9d6679fd9a607ced5845d
Jeny Susan Rajan <43616819+ceccs17d28@users.noreply.github.com>
2019-04-30 20:08:04 +0530

    Add files via upload

---
+++ kernel.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+curl -fLO "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.0.9.tar.xz"
+sudo apt install curl
+sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
+tar xvzf linux-5.0.9.tar.xz 
+cp /boot/config-4.19.0-kali4-amd64 .config
+make
+make modules_install
+sudo make install
