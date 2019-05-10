23f66a37ac32bdeafa6b653fa1ab64eb40e5d19e
Jeny Susan Rajan <43616819+ceccs17d28@users.noreply.github.com>
2019-05-10 23:37:41 +0530

    Task 4: scp,ssh,rsync

---
+++ Task 4/scpsshrsync.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+ssh cs17d28@192.168.0.30
+ls
+scp one.txt two.txt
+cat two.txt
+rsync -avzp /home/s4d1/b.txt cs17d28@192.168.0.30:
+~/.ssh
+rsync -zvp /home/s4d1/b.txt /home/s4d1/Desktop/a.txt
+ls -l
+
+exit
