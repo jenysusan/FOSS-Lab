2afe219f21a9e62eb04c3584e513d245cd58558e
Jeny Susan Rajan <43616819+ceccs17d28@users.noreply.github.com>
2019-05-10 23:16:00 +0530

    Task 1: Linux Commands

---
+++ Task 1/linuxcommands.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+ls
+ls -lt
+ls -ltr
+man ls
+ls -ab*
+alias ll='ls -l'
+ll
+unalias ll
+ll
+pwd
+cat >b.txt
+grep a a.txt
+grep a ?.txt
+grep a *.txt -c
+uname
+uname -a
+free
+free -m
+uname -r
+cat b.txt
+join a.txt b.txt
+ls
+ls -l ?.txt
+dhclient usb()
+
+exit
